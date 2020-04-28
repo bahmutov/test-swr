949c9c81bcf83d9bb2064a215bd5dd3036cdb338
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-28 14:51:24 -0400

    two tests and cache clear

--- src/profile-spec.js
+++ src/profile-spec.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import {mount} from 'cypress-react-unit-test'
 import Profile from './profile'
+import { SWRConfig, cache } from 'swr'
 
 describe('SWR profile', () => {
+  // to clear SWR cache in each test
+  // surround each test component with
+  // <SWRConfig value={{ dedupingInterval: 0 }}>
+  // https://github.com/zeit/swr/pull/231
+  beforeEach(() => {
+    cache.clear()
+  })
+
   it('loads data', () => {
     cy.stub(window, 'fetch').resolves({
       name: 'World'
     })
-    mount(<Profile />)
+    mount(<SWRConfig value={{ dedupingInterval: 0 }}>
+      <Profile />
+      </SWRConfig>
+    )
     cy.contains('hello World')
+  })
+
+  it('loads data with delay', () => {
+    cy.stub(window, 'fetch').resolves(
+      Cypress.Promise.resolve({ name: 'there' })
+      .delay(3000)
+    )
+
+    mount(<SWRConfig value={{ dedupingInterval: 0 }}>
+      <Profile />
+    </SWRConfig>)
+
+    cy.contains('loading...')
+    cy.contains('hello there')
+    cy.contains('loading...').should('not.exist')
   })
 })
